4fc13811de011915b2e5521b2620860483ec0151
abdias labrador <abdiaslabrador@gmail.com>
2024-12-02 08:05:57 +0100

    Dni is done with the dniInput and getDniWithLetter

--- src/main/java/dev/exerc/dni/App.java
+++ src/main/java/dev/exerc/dni/App.java
@@ -1,9 +1,16 @@
 package dev.exerc.dni;
 
+import java.util.Scanner;
+
+import dev.exerc.dni.models.Dni;
+
 public final class App {
     private App() {
     }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Dni dniLetter = new Dni();
+        System.out.println(dniLetter.getDniLetter(dniLetter.dniInput()));
+
     }
 }
---
+++ src/main/java/dev/exerc/dni/models/Dni.java
@@ -0,0 +1,29 @@
+package dev.exerc.dni.models;
+
+import java.util.Scanner;
+
+public class Dni {
+    char[] alphabet = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+    Scanner scanner = new Scanner(System.in);
+    
+    public int dniInput(){
+        int dniNumber = -1;
+        boolean flag =  false;
+        while(!flag || dniNumber < 0 || dniNumber > 99999999){
+            System.out.println("introduce el número DNI ( el número debe ser entre 0 y 99999999)");
+            try{
+                dniNumber = Integer.parseInt(scanner.nextLine());
+                flag = true;
+            }catch (NumberFormatException ex) {
+                System.out.println("el dato introducido es incorrecto");
+            }
+        }
+        return dniNumber;
+    }
+    public String getDniLetter(int numbDni){
+        if( numbDni >= 0 && numbDni <= 99999999){
+            return String.valueOf(numbDni) + alphabet[numbDni % 23];
+        }
+        return null;
+    }
+}
